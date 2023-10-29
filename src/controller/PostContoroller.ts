c5822af6e5d357e4286af540ce7368b8c483a8b6
tomo <tomo9634@gmail.com>
2023-10-29 15:43:38 +0700

    投稿作成時のリクエスト形式の修正

--- src/controller/PostContoroller.ts
+++ src/controller/PostContoroller.ts
@@ -5,7 +5,13 @@ export const PostContoroller = express.Router();
 
 PostContoroller.post('/', async (req, res, next) => {
   try {
-    const { body, userId } = req.body;
+    const { post } = req.body;
+
+    if (!post) {
+      return res.status(400).json({ message: 'post required for request parameters' });
+    }
+
+    const { body, userId } = post;
 
     if (!body) {
       throw new Error('post should not be empty');
