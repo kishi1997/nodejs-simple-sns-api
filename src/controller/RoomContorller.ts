3478577cfc4e01977ceeb4a1053d811791721e3f
tomo <tomo9634@gmail.com>
2024-04-06 01:04:05 +0900

    RoomAPIの修正

--- src/controller/RoomContorller.ts
+++ src/controller/RoomContorller.ts
@@ -1,34 +1,20 @@
-import express from "express";
-import { authAdmin, verifyToken } from "src/authMiddleware/auth";
-import { RoomService } from "src/service/RoomService";
-import { formatUserResponseWithoutEmail } from "src/utils/responseUtils/formatUserResponse";
+import express from 'express'
+import { authAdmin, verifyToken } from 'src/authMiddleware/auth'
+import { RoomService } from 'src/service/RoomService'
 
-export const RoomController = express.Router();
+export const RoomController = express.Router()
 RoomController.post('/', verifyToken, authAdmin, async (req, res, next) => {
-    try {
-        const { userIds } = req.body;
-        const roomData = await RoomService.createRoom(userIds, (req as any).userId);
-        const { roomUsers, newRoom } = roomData;
+  try {
+    const { userIds } = req.body
+    const roomData = await RoomService.createRoom(userIds, (req as any).userId)
+    const { roomInfo } = roomData
 
-        const formattedRoomUsers = roomUsers.map(user => formatUserResponseWithoutEmail(user));
-
-        const roomUserDetails = formattedRoomUsers.map((user) => {
-            return {
-                roomid: newRoom.id,
-                userId: user.id,
-                user: user
-            };
-        });
-
-        const responseData = {
-            id: newRoom.id,
-            messages: [],
-            roomUsers: roomUserDetails
-        };
-
-        res.json(responseData);
+    const responseData = {
+      room: roomInfo,
     }
-    catch (error) {
-        next(error);
-    }
-});
+
+    res.json(responseData)
+  } catch (error) {
+    next(error)
+  }
+})
--- src/service/RoomService.ts
+++ src/service/RoomService.ts
@@ -1,11 +1,15 @@
 import { Room } from 'src/entity/Room'
 import { User } from 'src/entity/User'
 import { createError } from 'src/utils/errorUtils/createError'
-import { getRoomRepository } from 'src/utils/getRepository'
+import {
+  getRoomRepository,
+  getRoomUserRepository,
+} from 'src/utils/getRepository'
 import { validateNull } from 'src/utils/validateUtils/validateNull'
 
 export class RoomService {
   static roomRepo = getRoomRepository()
+  static roomUserRepo = getRoomUserRepository()
 
   static validateRoomData(roomUserId: number) {
     validateNull({ name: 'roomUsersId', value: roomUserId, status: 422 })
@@ -14,16 +18,18 @@ export class RoomService {
   static async createRoom(roomUsersId: number[], userId: number) {
     roomUsersId.forEach(this.validateRoomData)
     const allUserIds = new Set([...roomUsersId, userId])
+    const roomUsers = await Promise.all(
+      Array.from(allUserIds).map(async userId => {
+        const user = await User.findOne({ where: { id: userId } })
+        if (user == null) {
+          throw createError('User does not exist', 422)
+        }
+        return user
+      })
+    )
     const formattedUserIds = Array.from(allUserIds)
       .sort((a, b) => a - b)
       .join('-')
-    const roomUsers = await User.find({
-      where: Array.from(allUserIds).map(id => ({ id })),
-    })
-
-    if (roomUsers == null) {
-      throw createError('User does not exist', 422)
-    }
     const existingRoom = await Room.findOne({
       where: { usersId: formattedUserIds },
     })
@@ -32,6 +38,30 @@ export class RoomService {
     }
     const newRoom = RoomService.roomRepo.create({ usersId: formattedUserIds })
     await RoomService.roomRepo.save(newRoom)
-    return { roomUsers, newRoom }
+    // ルームユーザーのデータの保存と取得
+    const roomUsersInfo = await Promise.all(
+      roomUsers.map(async roomUser => {
+        const roomUserData = RoomService.roomUserRepo.create({
+          userId: roomUser.id,
+          roomId: newRoom.id,
+        })
+        await RoomService.roomUserRepo.save(roomUserData)
+        return {
+          userId: roomUserData.userId,
+          roomId: roomUserData.roomId,
+          user: {
+            id: roomUser.id,
+            name: roomUser.name,
+            iconImageUrl: roomUser.iconImageUrl,
+          },
+        }
+      })
+    )
+    const roomInfo = {
+      id: newRoom.id,
+      messages: [],
+      roomUsers: roomUsersInfo,
+    }
+    return { roomInfo }
   }
 }
