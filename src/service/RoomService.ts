8d9b7f79c6611ad1e68ce1d42184e616b68b669a
tomo <tomo9634@gmail.com>
2024-04-06 01:56:00 +0900

    リファクタと修正

--- src/service/RoomService.ts
+++ src/service/RoomService.ts
@@ -14,19 +14,17 @@ export class RoomService {
   static validateRoomData(roomUserId: number) {
     validateNull({ name: 'roomUsersId', value: roomUserId, status: 422 })
   }
+  static async validateUser(userId: number) {
+    const user = await User.findOne({ where: { id: userId } })
+    if (user == null) {
+      throw createError('User does not exist', 422)
+    }
+    return user
+  }
 
   static async createRoom(roomUsersId: number[], userId: number) {
     roomUsersId.forEach(this.validateRoomData)
-    const allUserIds = new Set([...roomUsersId, userId])
-    const roomUsers = await Promise.all(
-      Array.from(allUserIds).map(async userId => {
-        const user = await User.findOne({ where: { id: userId } })
-        if (user == null) {
-          throw createError('User does not exist', 422)
-        }
-        return user
-      })
-    )
+    const allUserIds: number[] = Array.from(new Set([...roomUsersId, userId]))
     const formattedUserIds = Array.from(allUserIds)
       .sort((a, b) => a - b)
       .join('-')
@@ -36,27 +34,9 @@ export class RoomService {
     if (existingRoom !== null) {
       throw createError('Duplicate entry', 422)
     }
-    const newRoom = RoomService.roomRepo.create({ usersId: formattedUserIds })
-    await RoomService.roomRepo.save(newRoom)
-    // ルームユーザーのデータの保存と取得
-    const roomUsersInfo = await Promise.all(
-      roomUsers.map(async roomUser => {
-        const roomUserData = RoomService.roomUserRepo.create({
-          userId: roomUser.id,
-          roomId: newRoom.id,
-        })
-        await RoomService.roomUserRepo.save(roomUserData)
-        return {
-          userId: roomUserData.userId,
-          roomId: roomUserData.roomId,
-          user: {
-            id: roomUser.id,
-            name: roomUser.name,
-            iconImageUrl: roomUser.iconImageUrl,
-          },
-        }
-      })
-    )
+    const newRoom = this.roomRepo.create({ usersId: formattedUserIds })
+    await this.roomRepo.save(newRoom)
+    const roomUsersInfo = await getRoomUsersInfo(allUserIds, newRoom)
     const roomInfo = {
       id: newRoom.id,
       messages: [],
@@ -64,4 +44,26 @@ export class RoomService {
     }
     return { roomInfo }
   }
+}
+// ルームユーザーのデータの保存と取得
+async function getRoomUsersInfo(roomUsersId: number[], newRoom: Room) {
+  return Promise.all(
+    roomUsersId.map(async userId => {
+      const user = await RoomService.validateUser(userId)
+      const roomUserData = RoomService.roomUserRepo.create({
+        userId: user.id,
+        roomId: newRoom.id,
+      })
+      await RoomService.roomUserRepo.save(roomUserData)
+      return {
+        userId: roomUserData.userId,
+        roomId: roomUserData.roomId,
+        user: {
+          id: user.id,
+          name: user.name,
+          iconImageUrl: user.iconImageUrl,
+        },
+      }
+    })
+  )
 }
