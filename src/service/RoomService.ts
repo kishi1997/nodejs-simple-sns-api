48d7a7407f61fd6d5b16812202b2613bd0f835ae
tomo <tomo9634@gmail.com>
2024-05-13 00:35:45 +0900

    n+1問題の解決と、リレーションを使ってデータを取得するように修正

--- src/service/RoomService.ts
+++ src/service/RoomService.ts
@@ -2,46 +2,52 @@ import {
   getRoomRepository,
   getRoomUserRepository,
 } from 'src/utils/getRepository'
-import { getMessages } from 'src/utils/messageUtils/getMessageData'
-import { generateResponseRoomUsersInfo } from 'src/utils/roomUtils/generateResponseRoomUsersInfo'
 import { createNewRoom } from 'src/utils/roomUtils/createNewRoom'
-import { getUserData } from 'src/utils/userUtils/getUserData'
+import { RoomUser } from 'src/entity/RoomUser'
+import { Room } from 'src/entity/Room'
 
 export class RoomService {
   static roomRepo = getRoomRepository()
   static roomUserRepo = getRoomUserRepository()
   static async createRoom(roomUsersId: number[], userId: number) {
-    const allUserIds: number[] = Array.from(new Set([...roomUsersId, userId]))
-    const roomUsersData = await Promise.all(
-      allUserIds.map(async userId => {
-        return getUserData(userId)
-      })
-    )
+    const allUserIds = Array.from(new Set([...roomUsersId, userId]))
     const formattedUserIds = Array.from(allUserIds)
       .sort((a, b) => a - b)
       .join('-')
     const newRoom = await createNewRoom(formattedUserIds)
+    const roomWithMessages = await Room.findOne({
+      where: { id: newRoom.id },
+      relations: ['messages'],
+    })
     // roomUserテーブルにユーザー登録
-    await Promise.all(
-      allUserIds.map(async userId => {
-        const roomUser = this.roomUserRepo.create({
-          userId: userId,
-          roomId: newRoom.id,
-        })
-        await this.roomUserRepo.save(roomUser)
-      })
-    )
-    // 作成したルーム内のメッセージを取得
-    const messages = await getMessages(newRoom)
-    // レスポンスとして返すルームユーザーデータの取得及び整形
-    const roomUsersInfo = await generateResponseRoomUsersInfo(
-      roomUsersData,
-      newRoom
-    )
+    const roomUserRegistrationData = allUserIds.map((userId: number) => {
+      return {
+        userId: userId,
+        roomId: newRoom.id,
+      }
+    })
+    await RoomUser.createQueryBuilder()
+      .insert()
+      .into(RoomUser)
+      .values(roomUserRegistrationData)
+      .execute()
+
+    const roomUsers = await RoomUser.createQueryBuilder('roomUser')
+      .leftJoinAndSelect('roomUser.user', 'user')
+      .select([
+        'roomUser.roomId',
+        'roomUser.userId',
+        'user.id',
+        'user.name',
+        'user.iconImageUrl',
+      ])
+      .where('roomUser.roomId = :roomId', { roomId: newRoom.id })
+      .getMany()
+
     return {
       id: newRoom.id,
-      messages,
-      roomUsers: roomUsersInfo,
+      messages: roomWithMessages?.messages,
+      roomUsers: roomUsers,
     }
   }
 }
