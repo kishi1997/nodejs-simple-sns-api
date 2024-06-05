17a94bfd28d8199a846710995a3ce797c46dadbe
tomo <tomo9634@gmail.com>
2024-06-06 00:05:33 +0900

    deleteエンドポイントの追加

--- src/controller/PostContoroller.ts
+++ src/controller/PostContoroller.ts
@@ -2,6 +2,7 @@ import * as express from 'express'
 import { authAdmin, verifyToken } from 'src/authMiddleware/auth'
 import { Post } from 'src/entity/Post'
 import { PostService } from 'src/service/PostService'
+import { createError } from 'src/utils/errorUtils/createError'
 import { formatPostResponse } from 'src/utils/responseUtils/formatPostResponse'
 
 export const PostContoroller = express.Router()
@@ -36,3 +37,21 @@ PostContoroller.get('/:id', verifyToken, authAdmin, async (req, res, next) => {
     next(error)
   }
 })
+PostContoroller.delete(
+  '/:id',
+  verifyToken,
+  authAdmin,
+  async (req, res, next) => {
+    try {
+      const postId = req.params.id
+      const postIdNumber = parseInt(postId, 10)
+      const result = await Post.delete({ id: postIdNumber })
+      if (result.affected == null || result.affected === 0) {
+        throw createError('Failed to delete post', 422)
+      }
+      res.json({ success: true })
+    } catch (error) {
+      next(error)
+    }
+  }
+)
