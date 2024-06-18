b287af7a368ae5e4f87873421f1adf5761b9e18c
tomo <tomo9634@gmail.com>
2024-06-18 11:20:26 +0900

    req等に型を指定するように修正

--- src/controller/PostContoroller.ts
+++ src/controller/PostContoroller.ts
@@ -1,4 +1,5 @@
-import * as express from 'express'
+import express from 'express'
+import { Request, Response, NextFunction } from 'express'
 import { authAdmin, verifyToken } from 'src/authMiddleware/auth'
 import { Post } from 'src/entity/Post'
 import { PostService } from 'src/service/PostService'
@@ -6,23 +7,38 @@ import { formatPostResponse } from 'src/utils/responseUtils/formatPostResponse'
 
 export const PostContoroller = express.Router()
 
-PostContoroller.post('/', verifyToken, authAdmin, async (req, res, next) => {
-  try {
-    const { post } = req.body
-    const newPost = await PostService.createPost(post, (req as any).userId)
-    res.json({ post: formatPostResponse(newPost) })
-  } catch (error) {
-    next(error)
+PostContoroller.post(
+  '/',
+  verifyToken,
+  authAdmin,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { post } = req.body
+      const userId = req.userId
+      if (userId == null) {
+        res.json({ user: null })
+        return
+      }
+      const newPost = await PostService.createPost(post, userId)
+      res.json({ post: formatPostResponse(newPost) })
+    } catch (error) {
+      next(error)
+    }
   }
-})
-PostContoroller.get('/', verifyToken, authAdmin, async (req, res, next) => {
-  try {
-    const posts = await PostService.getPosts(req.query)
-    const formattedPostsData = posts.map((post: Post) => {
-      return formatPostResponse(post)
-    })
-    res.json({ posts: formattedPostsData })
-  } catch (error) {
-    next(error)
+)
+PostContoroller.get(
+  '/',
+  verifyToken,
+  authAdmin,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const posts = await PostService.getPosts(req.query)
+      const formattedPostsData = posts.map((post: Post) => {
+        return formatPostResponse(post)
+      })
+      res.json({ posts: formattedPostsData })
+    } catch (error) {
+      next(error)
+    }
   }
-})
+)
