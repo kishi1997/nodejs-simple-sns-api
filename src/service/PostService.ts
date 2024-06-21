7d64a2f47a7eb92e68fc3f23c6e95830e99e9257
tomo <tomo9634@gmail.com>
2024-06-21 12:33:21 +0900

    pagination処理の切り出しと、typeの修正

--- src/service/PostService.ts
+++ src/service/PostService.ts
@@ -2,7 +2,16 @@ import { validateEmpty } from 'src/utils/validateUtils/validateEmpty'
 import { getPostRepository } from '../utils/getRepository'
 import { validateNull } from 'src/utils/validateUtils/validateNull'
 import { Post } from 'src/entity/Post'
-import { GetPostsParams } from 'src/types/params.type'
+import { PaginationParams } from 'src/types/paginationParams.type'
+import { applyPagination } from 'src/utils/paginationUtils'
+
+type FilterParams = {
+  userId?: number
+}
+type GetPostsParams = {
+  pagination?: PaginationParams
+  filter?: FilterParams
+}
 
 export class PostService {
   static postRepo = getPostRepository()
@@ -25,24 +34,20 @@ export class PostService {
     return newPostData
   }
   static async getPosts(params: GetPostsParams): Promise<Post[]> {
-    // cursor,isNext,sizeにはundefinedの場合、初期値を設定
-    const {
-      pagination: { cursor, isNext = true, size = 50, order = 'DESC' } = {},
-      filter: { userId } = {},
-    } = params
-    // 初期値のあるデータをメソッドで指定
-    let query = Post.createQueryBuilder('post')
-      .leftJoinAndSelect('post.user', 'user')
-      .orderBy('post.id', order)
-      .limit(size)
-    // userId又はcursorデータがundefinedではない場合にクエリーにメソッドを追加
+    const { pagination = {}, filter: { userId } = {} } = params
+
+    let query = Post.createQueryBuilder('post').leftJoinAndSelect(
+      'post.user',
+      'user'
+    )
+
     if (userId !== undefined) {
       query = query.where('post.userId = :userId', { userId })
     }
-    if (cursor !== undefined) {
-      const comparison = isNext ? '<' : '>'
-      query = query.andWhere('post.id ' + comparison + ' :cursor', { cursor })
-    }
+
+    // ページネーションを適用
+    query = applyPagination(query, pagination)
+
     const posts = await query.getMany()
     return posts
   }
