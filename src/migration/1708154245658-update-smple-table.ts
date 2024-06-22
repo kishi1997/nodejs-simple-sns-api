b44b23d6bc33b8c3902d724ee247724831c94675
tomo <tomo9634@gmail.com>
2024-06-22 20:01:18 +0900

    マイグレーションファイル再度修正

--- src/migration/1708154245658-update-smple-table.ts
+++ src/migration/1708154245658-update-smple-table.ts
@@ -4,11 +4,20 @@ export class UpdateSmpleTable1708154245658 implements MigrationInterface {
     name = 'UpdateSmpleTable1708154245658'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`user\`
+        ADD \`createdDate\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+        ADD \`updatedDate\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)`)
+        await queryRunner.query(`ALTER TABLE \`post\` ADD \`createdDate\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)`);
+        await queryRunner.query(`ALTER TABLE \`post\` DROP COLUMN \`createdAt\``);
         await queryRunner.query(`CREATE TABLE \`roomUser\` (\`id\` int NOT NULL AUTO_INCREMENT, \`userId\` int NOT NULL, \`roomId\` varchar(255) NOT NULL, \`createdDate\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedDate\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP TABLE \`roomUser\``);
+        await queryRunner.query(`ALTER TABLE \`post\` DROP COLUMN \`createdDate\``);
+        await queryRunner.query(`ALTER TABLE \`post\` ADD \`createdAt\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP`);
+        await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`createdDate\``);
+        await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`updatedDate\``);
     }
 
 }
